54244c8e5bcedbe3996c2c64c7dfed10d9c86c15
Fraser Watt <fsrwatt@gmail.com>
2024-12-28 09:32:58 +0000

    Get latest bandcamp article (not just album of the day)

--- src/parser/bandcamp.rs
+++ src/parser/bandcamp.rs
@@ -1,7 +1,7 @@
 use scraper::{Html, Selector};
 use std::error::Error;
 
-pub fn get_latest_entry_url(html: &str, slug: &str) -> Result<Option<String>, Box<dyn Error>> {
+pub fn get_latest_entry_url(html: &str, slug: &str) -> Result<Option<(String, String)>, Box<dyn Error>> {
     // Get html into a html parser
     let document = Html::parse_document(html);
 
@@ -9,14 +9,32 @@ pub fn get_latest_entry_url(html: &str, slug: &str) -> Result<Option<String>, Bo
     let select_latest = Selector::parse("#p-daily-franchise > articles-list > *:nth-child(1) > a")?;
     // Create selector to extract the URL from the above element.
     let url_option = document
-        .select(&select_latest).find_map(|element| element.value().attr("href"))
-        .map(|u| u.to_string().replace(slug, ""));
+    .select(&select_latest).find_map(|element| element.value().attr("href"))
+    .map(|u| u.to_string().replace(slug, ""));
+
+    // Get the publication date from the html
+    let date_info_box = Selector::parse(".article-info-text:first-of-type")?;
+    let date_html = document
+        .select(&date_info_box)
+        .find_map(|element| Some(element.inner_html().to_string()))
+        .expect("No publication date found!");
+    let date_vec: Vec<_> = date_html
+        .split('\n')
+        .map(str::trim_start)
+        .filter(|s| !s.is_empty())
+        .collect();
+    let date = date_vec.last()
+        .ok_or("No date found in elements")?
+        .to_string();
+    println!("{:#?}", date);
+
+    // Return the URL and publication date
     match url_option {
-        Some(url) if !url.is_empty() => Ok(Some(url)),
+        Some(url) if !url.is_empty() => Ok(Some((url, date))),
         _ => Err("No entry found today!".into()),
     }
-}
 
+}
 pub fn get_latest_title_paragraph(html: &str) -> Result<(String, String), Box<dyn Error>> {
     let document = Html::parse_document(html);
     // Grab first paragraph of article
--- src/stories/bandcamp.rs
+++ src/stories/bandcamp.rs
@@ -1,38 +1,49 @@
 use crate::parser::{bandcamp, fetch};
 use crate::stories::story::{NewsSource, Story};
 use std::error::Error;
+use chrono::NaiveDate;
 
 pub async fn fetch_bandcamp_daily() -> Result<Story, Box<dyn Error>> {
+    // Want to take the latest article from one of these pages.
     let base_urls = [
         "https://daily.bandcamp.com/features",
         "https://daily.bandcamp.com/lists",
-        "https://daily.bandcamp.com/best-of-2025",
+        // "https://daily.bandcamp.com/best-of-2025",
         "https://daily.bandcamp.com/album-of-the-day",
-        ];
+    ];
 
-        for base_url in base_urls {
-            let webpage = fetch::get_html_body(base_url).await?;
-            let slug: String = base_url.to_string().replace("https://daily.bandcamp.com", "");
+    // Vector for selecting latest article.
+    let mut articles = Vec::new();
 
-            if let Ok(Some(daily_url)) = bandcamp::get_latest_entry_url(&webpage, &slug) {
-                let url = format!("{base_url}{daily_url}");
-                let daily_html = fetch::get_html_body(&url).await?;
+    for base_url in base_urls {
+        let webpage = fetch::get_html_body(base_url).await?;
+        let slug: String = base_url.to_string().replace("https://daily.bandcamp.com", "");
 
-                match bandcamp::get_latest_title_paragraph(&daily_html) {
-                    Ok((title, content)) => {
-                        return Ok(Story {
-                            title,
-                            url,
-                            content,
-                            news_source: NewsSource::Bandcamp,
-                        })
-                    },
-                    Err(e) => {
-                        println!("{e:?}");
-                    }
+        if let Ok(Some((slug_url, pub_date_str))) = bandcamp::get_latest_entry_url(&webpage, &slug) {
+            let article_url = format!("{base_url}{slug_url}");
+            let pub_date = NaiveDate::parse_from_str(&pub_date_str, "%B %d, %Y")
+                .expect("Failed to parse date");
+
+            let daily_html = fetch::get_html_body(&article_url).await?;
+
+            match bandcamp::get_latest_title_paragraph(&daily_html) {
+                Ok((title, content)) => {
+                    articles.push((pub_date, Story {
+                        title,
+                        url: article_url,
+                        content,
+                        news_source: NewsSource::Bandcamp,
+                    }));
+                },
+                Err(e) => {
+                    println!("{e:?}");
                 }
             }
         }
-
-        Err("No entry for today. Sad!".into())
     }
+    // Return the most recent article, or an error if none were found
+    match articles.into_iter().max_by_key(|(date, _)| *date) {
+        Some((_, latest_story)) => Ok(latest_story),
+        None => Err("No valid entries found. Sad!".into())
+    }
+}
